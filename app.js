1de0ea58d7f9076d690db4f7a52ea33fdfd42c6d
Lexie Chen <xiaoyuchen.uncc@gmail.com>
2022-03-05 22:05:29 -0800

    add bcrypt for password

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
-var md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -37,9 +37,10 @@ app.get('/register', function(req, res){
 })
 
 app.post('/register', function(req, res){
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
     const newUser = new User({
       email: req.body.username,
-      password: md5(req.body.password)
+      password: hash
     });
 
     newUser.save(function(err){
@@ -49,21 +50,23 @@ app.post('/register', function(req, res){
         res.render('secrets')
       }
     })
-  }
-);
+  });
+});
 
 app.post('/login', function(req, res) {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({email: username}, function(err, foundUser){
     if (err) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render('secrets');
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true) {
+            res.render('secrets');
+          }
+        });
       }
     }
   })
